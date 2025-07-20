cc814c62ab2a871882d0cf2a9f98f026b9412248
Vitngu <enda.wannafly@gmail.com>
2025-07-20 14:21:08 +0700

    lazy loading html

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const images = document.querySelectorAll('img');
-  
-    images.forEach(image => {
-      if (!image.hasAttribute('loading')) {
-        image.setAttribute('loading', 'lazy');
-      }
-    });
-  });
-
-document.addEventListener('DOMContentLoaded', function() {
     const images = document.querySelectorAll('img:not([alt])');
     const AltText = "© Cao Hải Đăng";
   
